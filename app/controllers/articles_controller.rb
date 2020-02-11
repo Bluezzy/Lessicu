2281a347f1fe74d450a3dbdc43143d6b70eeb1cd
Marc'Andria <marcandria@marcandria.home>
2020-02-11 23:17:07 +0100

    styling blog

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,35 @@ class ArticlesController < AdminController
     def show
         @article = Article.find(params[:id])
     end
+
+    def edit
+        @article = Word.find(params[:id])
+    end
+
+    def update
+        @article = Word.find(params[:id])
+        if @article.update_attributes(params.require(:article).permit(:author, :title, :content, :category))
+            redirect_to articles_path, flash: { success: "Modifié avec succès" }
+        else
+            redirect_to articles_path,  flash: { error: "Une erreur est survenue"}
+        end
+    end
+
+    def create
+        @word = Word.new(params.require(:word).permit(:name, :translation, :category))
+        if @word.save
+            redirect_to words_path, flash: { success: "Ajouté avec succès" }
+        else
+            redirect_to words_path, flash: { error: "Une erreur est survenue" }
+        end
+    end
+
+    def new
+        @word = Word.new
+    end
+
+    def destroy
+        Word.destroy(params[:id])
+        redirect_to words_path, flash: { success: "Supprimé avec succès" }
+    end
 end
--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,20 +1,22 @@
 class WordsController < AdminController
-    before_filter :find_word, only: [:show, :edit, :update]
     def index
-        @words = Word.order(:category).order(:name)
+        @words = Word.order(:name)
     end
 
     def show
+        @word = Word.find(params[:id])
     end
 
     def edit
+        @word = Word.find(params[:id])
     end
 
     def update
+        @word = Word.find(params[:id])
         if @word.update_attributes(params.require(:word).permit(:name, :translation, :category))
             redirect_to words_path, flash: { success: "Modifié avec succès" }
         else
-            redorect_to words_path,  flash: { error: "Une erreur est survenue"}
+            redirect_to words_path,  flash: { error: "Une erreur est survenue"}
         end
     end
 
@@ -34,11 +36,5 @@ class WordsController < AdminController
     def destroy
         Word.destroy(params[:id])
         redirect_to words_path, flash: { success: "Supprimé avec succès" }
-    end
-
-    protected
-
-    def find_word
-        @word = Word.find(params[:id])
     end
 end
